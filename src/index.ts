5960970cbf1eb4261c24bd52c591b15aaabe1112
OttGod12 <130059670+OttGod12@users.noreply.github.com>
2024-09-26 11:57:49 -0300

    n finalizado

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,44 @@
-import Display from "./Display";
- 
-const d = new Display();
+export interface IDisplay{
+    showNumber(value:number): void
+    clear(): void;
+}
 
+
+export default class Display implements IDisplay{
+    showNumber(value: number): void {
+
+    console.log(value);
+   
+    }
+    clear(): void {
+    }
+   }
+
+
+class MinhaDisplay extends Display{
+    showAlpha(value: string): void {
+        console.log("[a]" + value)
+    }
+    
+    override showNumber(value:number): void{
+        console.log("[n]" + value)
+    }
+}
+
+const d = new Display();
 d.showNumber(123.45);
+
+var d1 = new MinhaDisplay()
+d1.showNumber(123.45);
+
+
+var d2: Display = new MinhaDisplay()
+d2 = d1
+d1 = d2 as MinhaDisplay
+
+
+
+
 
 
 
